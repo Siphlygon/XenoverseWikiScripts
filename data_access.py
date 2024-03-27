d3597b657fba71bfd1b04372b4a07b75e0edbcf1
Ashley Parr <amparr83@gmail.com>
2024-03-27 23:18:40 +0000

    Reformat docstrings

--- data_access.py
+++ data_access.py
@@ -12,6 +12,10 @@ def gender_code(gender):
     """
     Accesses dictionary of gender codes.
 
+    Pokémon have different rates of being either male or female. Pokémon may also be genderless, which is guaranteed if
+    applicable. This dictionary is used to convert the game's internal representation of a gender code into a number
+    representing the same for the wiki's display.
+
     :param string gender: The growth rate represented in pokemon.txt.
     :return string: The corresponding gender code.
     """
@@ -22,9 +26,11 @@ def gender_code(gender):
 
 def growth_rate(rate):
     """
-    Accesses a dictionary of growth rates. Pokémon are grouped into experience groups, which determine how much EXP they
-    need to level up. This dictionary is used to convert the game's internal representation of growth rates to a number,
-    which the wiki uses to display the growth rate in another format.
+    Accesses a dictionary of growth rates.
+
+    Pokémon are grouped into experience groups, which determine how much EXP they need to level up. This dictionary is
+    used to convert the game's internal representation of growth rates to a number, which the wiki uses to display the
+    growth rate in another format.
 
     Format: "GrowthRate" -> "EXPNumber"
 
@@ -38,9 +44,11 @@ def growth_rate(rate):
 
 def tm_info(move):
     """
-    Accesses a dictionary of TM info. TMs are stored as move names in tm.txt, but are converted to TM numbers (01-95)
-    for the wiki, which is what the reference dictionary is used for. The type of each TM and if the move can gain STAB
-    are also stored in this dictionary.
+    Accesses a dictionary of TM info.
+
+    TMs are stored as move names in tm.txt, but are converted to TM numbers (01-95) for the wiki, which is what the
+    reference dictionary is used for. The type of each TM and if the move can gain STAB are also stored in this
+    dictionary.
 
     Format: "TMName" -> "TMNo,canHaveSTAB,Type"
 
@@ -54,8 +62,10 @@ def tm_info(move):
 
 def move_info(move):
     """
-    Accesses a dictionary of move info. Every move in the wiki's move templates is stored in this dictionary, along with
-    its proper name, if it can gain STAB, and its type. This is used to format moves in the wiki page.
+    Accesses a dictionary of move info.
+
+    Every move in the wiki's move templates is stored in this dictionary, along with its proper name, if it can gain
+    STAB, and its type. This is used to format moves in the wiki page.
 
     Format: "MoveName" -> "RealMoveName,canHaveSTAB,type"
 
@@ -69,9 +79,11 @@ def move_info(move):
 
 def wild_item_info(item):
     """
-    Accesses a dictionary of wild item info. This does not account for every item in the game, just any item that can
-    appear as a wild held item. This is often stored in Italian or otherwise without spaces in the game's data, so this
-    dictionary is used to convert the game's internal representation to the English name.
+    Accesses a dictionary of wild item info.
+
+    This does not account for every item in the game, just any item that can appear as a wild held item. This is often
+    stored in Italian or otherwise without spaces in the game's data, so this dictionary is used to convert the game's
+    internal representation to the English name.
 
     Format: "ItemInternalName" -> "ItemDisplayName"
 
@@ -85,9 +97,11 @@ def wild_item_info(item):
 
 def pokemon_info(dex):
     """
-    Accesses a dictionary of Pokémon info. This is used to convert a dex number to its respective Pokémon name, which is
-    used in the wiki page. Dex number was chosen as the next and previous Pokémon are determined by adjusting this
-    number. This retrieves both the internal name and the display name of the Pokémon.
+    Accesses a dictionary of Pokémon info.
+
+    This is used to convert a dex number to its respective Pokémon name, which is used in the wiki page. Dex number was
+    chosen as the next and previous Pokémon are determined by adjusting this number. This retrieves both the internal
+    name and the display name of the Pokémon.
 
     Format: "DexNumber" -> "InternalName,DisplayName"
 
@@ -101,9 +115,11 @@ def pokemon_info(dex):
 
 def location_info(zone):
     """
-    Accesses a dictionary of location info. Locations consist of multiple numbered zones in the game, and this
-    dictionary matches said zones to English location names, which were manually translated both through software and by
-    cross-referencing the English wiki and the Italian wiki.
+    Accesses a dictionary of location info.
+
+    Locations consist of multiple numbered zones in the game, and this dictionary matches said zones to English location
+    names, which were manually translated both through software and by cross-referencing the English wiki and the
+    Italian wiki.
 
     Format: "ZoneNumber" -> "LocationRealName"
 
@@ -117,9 +133,11 @@ def location_info(zone):
 
 def ability_info(ability):
     """
-    Accesses a dictionary of ability info. Abilities are stored as fully capitalised names in the game data, and they
-    often require spaces inputted for display purposes. This dictionary is used for that exact purpose, and contains
-    every ability a Pokémon can have in Xenoverse.
+    Accesses a dictionary of ability info.
+
+    Abilities are stored as fully capitalised names in the game data, and they often require spaces inputted for display
+    purposes. This dictionary is used for that exact purpose, and contains every ability a Pokémon can have in
+    Xenoverse.
 
     Format: "AbilityInternalName" -> "AbilityDisplayName"
 
@@ -133,9 +151,10 @@ def ability_info(ability):
 
 def ability_immunities(ability):
     """
-    Accesses a dictionary of ability immunities. This dictionary contains all abilities that grant immunities to certain
-    types of moves, regardless of secondary effects. This is used to indicate which abilities do so for the purpose of
-    calculating type effectiveness.
+    Accesses a dictionary of ability immunities.
+
+    This dictionary contains all abilities that grant immunities to certain types of moves, regardless of secondary
+    effects. This is used to indicate which abilities do so for the purpose of calculating type effectiveness.
 
     Format: "AbilityInternalName" -> "ImmunityType"
 
@@ -149,10 +168,11 @@ def ability_immunities(ability):
 
 def static_encounters(pokemon):
     """
-    Accesses a dictionary of static encounters. Said dictionary has unreachable elements (namely, Alolan forms) for the
-    purpose of record, and possible future implementation. This dictionary is used to determine the type of static
-    encounter a Pokémon has, which can be either a gift, a trade, or a battle. This is always displayed for any
-    applicable Pokémon.
+    Accesses a dictionary of static encounters.
+
+    Said dictionary has unreachable elements (namely, Alolan forms) for the purpose of record, and possible future
+    implementation. This dictionary is used to determine the type of static encounter a Pokémon has, which can be either
+    a gift, a trade, or a battle. This is always displayed for any applicable Pokémon.
 
     Format: "PokemonInternalName" -> "LocationName,EncounterType"
 
